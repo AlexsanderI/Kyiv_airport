fe6bcab3d555046354afea035dc48e5cffd162ff
Your Name <y.y.oleksander@gmail.com>
2022-09-15 22:04:41 +0300

    3

--- src/table/sac.jsx
+++ src/table/sac.jsx
@@ -3,66 +3,43 @@ import './boardTable.scss';
 
 const BoardTable = () => {
   return (
-    <div class="table">
-      <div class="table-header">
-        <div class="header__item">
-          <a id="name" class="filter__link">
-            Terminal
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="wins" class="filter__link filter__link--number">
-            Local time
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="total" class="filter__link filter__link--number">
-            Destination
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="wins" class="filter__link filter__link--number">
-            Status
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="draws" class="filter__link filter__link--number">
-            Airline
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="losses" class="filter__link filter__link--number">
-            Flight
-          </a>
-        </div>
-      </div>
-      <div class="table-content">
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data">1:20</div>
-          <div class="table-data">Warsaw</div>
-          <div class="table-data">Landed 1:28</div>
-          <div class="table-data">LOT</div>
-          <div class="table-data">LO755</div>
-        </div>
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data"> 1:45</div>
-          <div class="table-data">Cologne</div>
-          <div class="table-data">Landed 1:43</div>
-          <div class="table-data">Wizz Air</div>
-          <div class="table-data">W67308</div>
-        </div>
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data"> 2:45</div>
-          <div class="table-data"> Paris (Beauvais)</div>
-          <div class="table-data">Landed 2:06</div>
-          <div class="table-data">Wizz Air</div>
-          <div class="table-data">W67308</div>
-        </div>
-      </div>
-    </div>
+    <tbody>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      <tr>
+        <td>Ernst Handel</td>
+        <td>Roland Mendel</td>
+        <td>Austria</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>Helen Bennett</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Laughing Bacchus Winecellars</td>
+        <td>Yoshi Tannamuri</td>
+        <td>Canada</td>
+      </tr>
+      <tr>
+        <td>Magazzini Alimentari Riuniti</td>
+        <td>Giovanni Rovelli</td>
+        <td>Italy</td>
+      </tr>
+    </tbody>
   );
 };
 
