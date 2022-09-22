8404e14b4755d3f239d5bcc26fe629162998970a
Your Name <y.y.oleksander@gmail.com>
2022-09-22 04:35:56 +0300

    2

--- src/date/DateBorder.jsx
+++ src/date/DateBorder.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import BoardTable from '../table/BoardTable';
 import './date.scss';
+import moment, { calendarFormat } from 'moment';
+import { useState, useEffect } from 'react';
 
 const DateBorder = () => {
+  let createdDate = moment(new Date()).format();
+  let tomorrow = moment(createdDate).add(1, 'd');
+  let yestarday = moment(createdDate).subtract(1, 'd');
+  const [calendarFormat, changeDate] = useState({
+    date: moment(new Date()).format(),
+  });
+
+  const { date } = calendarFormat;
+
+  const handleChangeDate = event => {
+    changeDate({ ...calendarFormat, [event.target.name]: event.target.value });
+  };
+
+  console.log(moment(date).format('DD/MM'));
   return (
     <div className="board__date">
       <div className="date__select">
-        <input type="date" name="date" className="date__form"></input>
+        <input
+          type="date"
+          name="date"
+          className="date__form"
+          onChange={handleChangeDate}
+          value={date}
+        ></input>
         <div className="date__icon">
-          <div className="date__icon-text">14/09</div>
+          <div className="date__icon-text">{moment(date).format('DD/MM')}</div>
           <div className="date__icon-png"></div>
         </div>
         <div className="date__days">
           <div className="date__days-box yestarday">
-            <div className="date__days-num">13/09</div>
+            <div className="date__days-num">{yestarday.format('DD/MM')}</div>
             <div className="date__days-text">Yestarday</div>
           </div>
           <div className="date__days-box today">
-            <div className="date__days-num">14/09</div>
+            <div className="date__days-num">{moment(new Date()).format('DD/MM')}</div>
             <div className="date__days-text">Today</div>
           </div>
           <div className="date__days-box tomorrow">
-            <div className="date__days-num">15/09</div>
+            <div className="date__days-num">{tomorrow.format('DD/MM')}</div>
             <div className="date__days-text">Tomorrow</div>
           </div>
         </div>
--- src/table/BoardTable.jsx
+++ src/table/BoardTable.jsx
@@ -3,66 +3,84 @@ import './boardTable.scss';
 
 const BoardTable = () => {
   return (
-    <div class="table">
-      <div class="table-header">
-        <div class="header__item">
-          <a id="name" class="filter__link">
-            Terminal
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="wins" class="filter__link filter__link--number">
-            Local time
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="total" class="filter__link filter__link--number">
-            Destination
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="wins" class="filter__link filter__link--number">
-            Status
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="draws" class="filter__link filter__link--number">
-            Airline
-          </a>
-        </div>
-        <div class="header__item">
-          <a id="losses" class="filter__link filter__link--number">
-            Flight
-          </a>
-        </div>
-      </div>
-      <div class="table-content">
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data">1:20</div>
-          <div class="table-data">Warsaw</div>
-          <div class="table-data">Landed 1:28</div>
-          <div class="table-data">LOT</div>
-          <div class="table-data">LO755</div>
-        </div>
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data"> 1:45</div>
-          <div class="table-data">Cologne</div>
-          <div class="table-data">Landed 1:43</div>
-          <div class="table-data">Wizz Air</div>
-          <div class="table-data">W67308</div>
-        </div>
-        <div class="table-row">
-          <div class="table-data">A</div>
-          <div class="table-data"> 2:45</div>
-          <div class="table-data"> Paris (Beauvais)</div>
-          <div class="table-data">Landed 2:06</div>
-          <div class="table-data">Wizz Air</div>
-          <div class="table-data">W67308</div>
-        </div>
-      </div>
-    </div>
+    <table className="styled-table">
+      <thead>
+        <tr className="table__title">
+          <td>Terminal</td>
+          <td>Local time</td>
+          <td>Destination</td>
+          <td>Status</td>
+          <td>Airline</td>
+          <td>Flight</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+        <tr className="table__sheduel">
+          <td>A</td>
+          <td>1:20</td>
+          <td>Warsaw</td>
+          <td>Landed 1:28</td>
+          <td>LOT</td>
+          <td>LO755</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
